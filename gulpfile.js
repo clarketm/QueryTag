907b66b90f39e2ef3bde0b4cf29d5e6dec59cbc2
Travis Clarke <travis.m.clarke@gmail.com>
2016-03-05 22:44:23 -0500

    added gulpfile.js and .travis.yml

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    rename = require("gulp-rename"),
+    del = require('del'),
+    js = require('gulp-uglify');
+
+gulp.task('js', ['clean'], function () {
+    return gulp.src('./src/stable/js/querytag.js')
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(js({output: {comments: /^!|@preserve|@license|@cc_on/i}}))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist/js/'));
+});
+gulp.task('clean', function () {
+    return del(['dist/js']);
+});
+
+gulp.task('build', ['js']);
+
+gulp.task('default', ['build']);
+
+
+
